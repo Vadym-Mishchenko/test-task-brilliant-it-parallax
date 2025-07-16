da1cdc3418f8baf9605f65ce98b506e768a37016
Michman <michmanvadik@gmail.com>
2025-07-16 15:12:39 +0300

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // ✅ это важно
+      '@': path.resolve(__dirname, 'src'),
     },
   },
+  base: '/test-task-brilliant-it-parallax/',
 });
